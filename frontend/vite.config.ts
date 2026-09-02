850afe564de3ef496a8e3209522eebc453f69eba
Andrei Pana <panaandreimihai@gmail.com>
2026-09-02 22:47:17 +0300

    fix: allow all hosts for vite preview in production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,9 @@ import viteReact from '@vitejs/plugin-react'
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [tanstackStart(), tailwindcss(), viteReact()],
+  preview: {
+    allowedHosts: true,
+  }
 })
 
 export default config
