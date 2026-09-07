1374ea531c201f7cb0592b77f40bd3a6d8b08814
Andrei Pana <panaandreimihai@gmail.com>
2026-09-07 09:55:35 +0300

    fix: revert google fonts deferral causing fallback font render

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -135,16 +135,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Work+Sans:wght@300;400;500;600&display=swap" />
-        <link 
-          rel="stylesheet" 
-          href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Work+Sans:wght@300;400;500;600&display=swap" 
-          media="print" 
-          onLoad={(e) => { (e.currentTarget as HTMLLinkElement).media = 'all' }} 
-        />
-        <noscript>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Work+Sans:wght@300;400;500;600&display=swap" />
-        </noscript>
+        <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Work+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
       </head>
       <body>
         <AuthProvider>
